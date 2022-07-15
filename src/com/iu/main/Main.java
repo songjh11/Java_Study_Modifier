34c6efc69753b26ff5496e663589bed813000b16
songjh11 <jhmelody91@gmail.com>
2022-07-15 15:49:48 +0900

    f

--- src/com/iu/main/Main.java
+++ src/com/iu/main/Main.java
@@ -1,6 +1,8 @@
 package com.iu.main;
 
 import com.iu.terran.Marine;
+import com.iu.terran.Scv;
+import com.iu.unit.Unit;
 import com.iu.zerg.Hydra;
 
 public class Main {
@@ -8,6 +10,21 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Marine m1 = new Marine();
+		Scv s1 = new Scv();
+		
+		//Marine is a Unit
+		//Scv is a Unit
+		
+		
+		Unit unit = m1;
+		
+		System.out.println(unit.getHp());
+		System.out.println(unit.getName());
+		System.out.println(unit.getColor());
+		
+		m1 = (Marine)unit;//unit이 scv일수도 있고 marine일수도 있기 때문에 앞에 표시해줌
+		System.out.println(m1.getWeapon());
+		
 		m1.setHp(30);
 		m1.setWeapon(null);
 		
